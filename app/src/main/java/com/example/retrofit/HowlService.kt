22ea4d7a0dc695f9b751c58f54b332a36fa705b5
Taehee <zmxncbvb123@gmail.com>
2020-08-15 21:51:56 +0900

    RetrofitServerConnect

---
+++ app/src/main/java/com/example/retrofit/HowlService.kt
@@ -0,0 +1,26 @@
+package com.example.retrofit
+
+import retrofit2.Call
+import retrofit2.http.*
+
+data class ResponseDTO(var result:String? = null)
+
+interface HowlService
+{
+    @GET("/howl")
+    fun getRequest(@Query("name") name: String): Call<ResponseDTO>
+
+    @GET("/howl/{id}")
+    fun getParamRequest(@Path("id")id :String):Call<ResponseDTO>
+    @FormUrlEncoded
+    @POST("/howl")
+    fun postRequest(@Field("id")id :String,
+                    @Field("password")password:String):Call<ResponseDTO>
+    @FormUrlEncoded
+    @PUT("/howl/{id}")
+    fun putRequest(@Path("id") id:String,
+                   @Field("content")content:String):Call<ResponseDTO>
+
+    @DELETE("/howl/{id}")
+    fun deleteRequest(@Path("id") id : String) : Call<ResponseDTO>
+}
